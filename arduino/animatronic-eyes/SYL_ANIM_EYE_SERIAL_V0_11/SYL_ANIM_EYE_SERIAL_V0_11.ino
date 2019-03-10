5d07dd39b21769f7bf3851b6a99f0864131819a5
QuantumCatX <denisefrom2018@gmail.com>
2019-03-10 21:16:59 +1300

    Add files via upload

--- arduino/animatronic-eyes/SYL_ANIM_EYE_SERIAL_V0_11/SYL_ANIM_EYE_SERIAL_V0_11.ino
+++ arduino/animatronic-eyes/SYL_ANIM_EYE_SERIAL_V0_11/SYL_ANIM_EYE_SERIAL_V0_11.ino
@@ -2,19 +2,37 @@
 
 Servo sg90_1;
 Servo sg90_2;
+Servo sg90_3;
+Servo sg90_4;
 Servo hx5010_1;
 Servo hx5010_2;
+Servo hx5010_3;
+Servo hx5010_4;
 
 void setup() {
   sg90_1.attach(10);
   sg90_2.attach(11);
+  
+  sg90_3.attach(6);
+  sg90_4.attach(7);
+  
   hx5010_1.attach(8);
-  hx5010_2.attach(9);  
+  hx5010_2.attach(9);
 
-  hx5010_1.write(0);
-  hx5010_2.write(0);
+  hx5010_3.attach(4);
+  hx5010_4.attach(5);
+
   sg90_1.write(45);
   sg90_2.write(45); 
+  
+  sg90_3.write(45);
+  sg90_4.write(45);
+  
+  hx5010_1.write(0);
+  hx5010_2.write(0);
+  
+  hx5010_3.write(90);
+  hx5010_4.write(90);
   
   Serial.begin(9600);
   Serial.setTimeout(10);
@@ -31,32 +49,47 @@ void loop() {
   
   if(servoState == 2){
     hx5010_1.write(0);
+    hx5010_3.write(90);    
   }
   else if(servoState == 3){
     hx5010_1.write(90);
+    hx5010_3.write(0);
   }  
   else if(servoState == 4){
     hx5010_2.write(0);
+    hx5010_4.write(90);
   } 
   else if (servoState == 5){
     hx5010_2.write(90);
+    hx5010_4.write(0);
   }
   else if (servoState == 6){
-    sg90_1.write(0);  p
+    sg90_1.write(0);
+    sg90_3.write(0); 
   }
   else if (servoState == 7){
-    sg90_1.write(90);  
+    sg90_1.write(90);
+    sg90_3.write(90);  
   }
   else if (servoState == 8){
-    sg90_2.write(0);  
+    sg90_2.write(0);
+    sg90_4.write(0);  
   }
   else if (servoState == 9){
-    sg90_2.write(90);  
+    sg90_2.write(90);
+    sg90_4.write(90);  
   }
   else if (servoState == 10){
     hx5010_1.write(0);
     hx5010_2.write(0);
+    
+    hx5010_3.write(90);
+    hx5010_4.write(90);
+    
     sg90_1.write(45);
-    sg90_2.write(45);  
+    sg90_2.write(45);
+    
+    sg90_3.write(45);
+    sg90_4.write(45); 
   }  
 }
