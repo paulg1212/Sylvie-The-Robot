6729cda1be86d7b23a41743f0d06a9bd9104229e
misses-robot <misses-robot@sylvie.net>
2019-04-12 23:57:59 +1200

    Added new arduino code.

--- arduino/animatronic-eyes/SYL_ANIM_EYE_SERIAL_V0_11/SYL_ANIM_EYE_SERIAL_V0_11.ino
+++ arduino/animatronic-eyes/SYL_ANIM_EYE_SERIAL_V0_11/SYL_ANIM_EYE_SERIAL_V0_11.ino
@@ -9,7 +9,13 @@ Servo hx5010_2;
 Servo hx5010_3;
 Servo hx5010_4;
 
+int led_blue = 3;
+int led_red = 2;
+
 void setup() {
+  pinMode(led_red, OUTPUT);
+  pinMode(led_blue, OUTPUT);
+  
   sg90_1.attach(10);
   sg90_2.attach(11);
   
@@ -36,6 +42,8 @@ void setup() {
   
   Serial.begin(9600);
   Serial.setTimeout(10);
+  
+  digitalWrite(led_blue, HIGH);  
 }
 
 long servoState = 0;
@@ -45,51 +53,60 @@ void loop() {
   servoState = Serial.parseInt();
   
   Serial.print("Received command: ");
-  Serial.println(servoState);  
+  Serial.println(servoState);    
+
+  if(servoState > 0) { 
   
-  if(servoState == 2){
-    hx5010_1.write(0);
-    hx5010_2.write(90);    
-  }
-  else if(servoState == 3){
-    hx5010_1.write(90);
-    hx5010_2.write(0);
-  }  
-  else if(servoState == 4){
-    hx5010_3.write(0);
-    hx5010_4.write(90);
-  } 
-  else if (servoState == 5){
-    hx5010_3.write(90);
-    hx5010_4.write(0);
-  }
-  else if (servoState == 6){
-    sg90_1.write(45);
-    sg90_2.write(45); 
-  }
-  else if (servoState == 7){
-    sg90_1.write(130);
-    sg90_2.write(130);  
-  }
-  else if (servoState == 8){
-    sg90_3.write(45);
-    sg90_4.write(45);  
-  }
-  else if (servoState == 9){
-    sg90_3.write(130);
-    sg90_4.write(130);  
-  }
-  else if (servoState == 10){
-    hx5010_1.write(0);
-    hx5010_2.write(0);
+    if(servoState == 2){
+      hx5010_1.write(0);
+      hx5010_2.write(90);       
+    }
+    else if(servoState == 3){
+      hx5010_1.write(90);
+      hx5010_2.write(0);  
+    }  
+    else if(servoState == 4){
+      hx5010_3.write(0);
+      hx5010_4.write(90);    
+    } 
+    else if (servoState == 5){
+      hx5010_3.write(90);
+      hx5010_4.write(0);   
+    }    
+    else if (servoState == 6){
+      sg90_1.write(45);
+      sg90_2.write(45);    
+    }
+    else if (servoState == 7){
+      sg90_1.write(130);
+      sg90_2.write(130);   
+    }
+    else if (servoState == 8){
+      sg90_3.write(45);
+      sg90_4.write(45);    
+    }
+    else if (servoState == 9){
+      sg90_3.write(130);
+      sg90_4.write(130);      
+    }
+    else if (servoState == 10){
+      hx5010_1.write(0);
+      hx5010_2.write(0);
     
-    hx5010_3.write(90);
-    hx5010_4.write(90);
+      hx5010_3.write(90);
+      hx5010_4.write(90);
     
-    sg90_1.write(90);
-    sg90_2.write(90);
+      sg90_1.write(90);
+      sg90_2.write(90);
     
-    sg90_3.write(90);
-    sg90_4.write(90); 
-  }  
+      sg90_3.write(90);
+      sg90_4.write(90); 
+    }
+
+    digitalWrite(led_red, HIGH);
+    digitalWrite(led_blue, LOW);
+    delay(250);
+    digitalWrite(led_red, LOW);    
+    digitalWrite(led_blue, HIGH); 
+  }
 }
